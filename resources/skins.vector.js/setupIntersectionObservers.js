c9873ef0f1423cd43099e4ddd35b997ec0002f84
Luka Simić <git@kocka.tech>
2024-12-26 20:15:23 +0100

    Reorganized titlebar and toolbar areas.

--- resources/skins.vector.js/setupIntersectionObservers.js
+++ resources/skins.vector.js/setupIntersectionObservers.js
@@ -100,7 +100,10 @@ function initStickyHeaderABTests( abConfig, isStickyHeaderFeatureAllowed, getEna
 const updateTocLocation = () => {
 	const isPinned = features.isEnabled( 'toc-pinned' );
 	const isStickyHeaderVisible = document.body.classList.contains( STICKY_HEADER_VISIBLE_CLASS );
+	/* UTW change: we make the header work on all widths
 	const isBelowDesktop = belowDesktopMedia.matches;
+	*/
+	const isBelowDesktop = false;
 
 	const pinnedContainerId = 'vector-toc-pinned-container';
 	const stickyHeaderUnpinnedContainerId = 'vector-sticky-header-toc-unpinned-container';
