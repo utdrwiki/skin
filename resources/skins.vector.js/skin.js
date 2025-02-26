4d2531b251e9835f80e416d5dcbb4fa47e844819
Luka Simić <git@kocka.tech>
2025-02-26 16:47:24 +0100

    Mobile navigation styles and functionality.

--- resources/skins.vector.js/skin.js
+++ resources/skins.vector.js/skin.js
@@ -14,6 +14,7 @@ const languageButton = require( './languageButton.js' ),
 	userPreferences = require( './userPreferences.js' ),
 	sidebarToggle = require( './sidebarToggle.js' ),
 	siteNotice = require( './siteNotice.js' ),
+	mobileNavigation = require(  './mobileNavigation.js' ),
 	{ isNightModeGadgetEnabled, disableNightModeForGadget, alterExclusionMessage, removeBetaNotice } = require( './disableNightModeIfGadget.js' ),
 	teleportTarget = /** @type {HTMLElement} */require( /** @type {string} */ ( 'mediawiki.page.ready' ) ).teleportTarget;
 
@@ -112,6 +113,7 @@ function main( window ) {
 	tables();
 	sidebarToggle();
 	siteNotice();
+	mobileNavigation();
 	// UTW change: cross-wiki login script
 	if ( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'Userlogin' ) {
 		require( './login.js' )();
