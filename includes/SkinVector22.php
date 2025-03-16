f5060b0ca7710a261e3362e5362adac0916930b8
Luka Simić <git@kocka.tech>
2025-03-17 00:47:30 +0100

    Added a Discussions button into the headers.

--- includes/SkinVector22.php
+++ includes/SkinVector22.php
@@ -453,6 +453,19 @@ class SkinVector22 extends SkinMustache {
 			$this,
 		);
 
+		$discussionsLink = $config->get( 'VectorDiscussionsLink' );
+		$discussionsButton = $discussionsLink ? new VectorComponentButton(
+			$this->msg( 'utw-discussions-button' )->text(),
+			'speechBubble',
+			null,
+			'discussions-button',
+			[],
+			'quiet',
+			'default',
+			true,
+			$discussionsLink,
+		) : null;
+
 		$components = $tocComponents + [
 			/* UTW change: edit button rework
 			'data-add-topic-button' => $hasAddTopicButton ? new VectorComponentButton(
@@ -506,6 +519,7 @@ class SkinVector22 extends SkinMustache {
 				$localizer
 			),
 			'data-main-menu' => $mainMenu,
+			'data-discussions-button' => $discussionsButton,
 			'data-main-menu-dropdown' => new VectorComponentDropdown(
 				VectorComponentMainMenu::ID . '-dropdown',
 				$this->msg( VectorComponentMainMenu::ID . '-label' )->text(),
